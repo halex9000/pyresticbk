098db6bf729aae7451a0d31556c9170184c28138
Alex <halex9000@gmail.com>
2024-05-20 14:42:12 +0200

    Update pyResticBK.py

--- pyResticBK.py
+++ pyResticBK.py
@@ -1,14 +1,27 @@
 #####################################################################################################
 PROGRAM = "pyResticBK.py"
 #  Author: Alessandro Carichini 
-#    Date: 03-05-2024
-VERSION = "1.2405c"
+#    Date: 02-05-2024
+#  Update: 06-05-2024
+VERSION = "1.2405_10a"
 #    Note: Automate restic backup using a config file
 #
 #  params: backup = Start backup all lines inside myfile_config
 #          status = Show snapshots and stats for all repository inside myfile_config
 #          clean =  Clean repository using retention policy inside myfile_config. Example:
 #                  --keep-daily 10 --keep-weekly 4 --keep-monthly 6
+#####################################################################################################
+#    Todo: 
+#       - Send file log to email
+#       - Check Repository disk space 
+#       - Check Integrity Repository 
+#####################################################################################################
+# restic usage:
+#
+# restic -r /path_repo init
+# restic -r /path_repo backup /path_source  
+# restic -r /path_repo stats 
+# 
 #####################################################################################################
 #
 
@@ -26,7 +39,7 @@ def WriteLogFile(filename,buffer=None):
     date_obj = datetime.now()
     log_date = date_obj.strftime("%Y-%m-%d %H:%M.%S")
     if buffer is None:
-        fhandle = open(filename, 'w+')
+        fhandle = open(filename, 'a')
         return fhandle
     else:
         filename.write(f"{log_date};{buffer}\n")
@@ -36,9 +49,9 @@ def ResticExec(restic_bin, path_src, path_repo, param_string):
     command_list = shlex.split(param_string)
 
     if command_list[0] == "backup":
-        command = [restic_bin, "-r", path_repo, "-q", "backup", path_src ]
+        command = [restic_bin, '-r', path_repo, '-q', 'backup', path_src ]
     else:
-        command = [restic_bin, "-r", path_repo ]
+        command = [restic_bin, '-r', path_repo ]
         command.extend(command_list)
 
     try:
@@ -49,10 +62,14 @@ def ResticExec(restic_bin, path_src, path_repo, param_string):
 
 def GetDirSize(directory):
     total_size = 0
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
+    for dirpath, _, filenames in os.walk(directory):
+        for filename in filenames:
+            fp = os.path.join(dirpath, filename)
+            try:
+                total_size += os.stat(fp).st_size
+            except:
+                total_size += 0
+                
     return total_size / (1024 ** 3)
 
 #####################################################################################################
@@ -100,15 +117,16 @@ else:
                             # Check Platform
                             if OS == "Windows":
                                 RESTIC_EXE = config_dict["restic_win"]
-                                SLASH = "\\"
                             else:
                                 RESTIC_EXE = config_dict["restic_linux"]
-                                SLASH = "/"
 
                             head_log = f"{PARAM};{PATH_REPO}"
 
+                            print(f"working: {PARAM} into {PATH_REPO}")
+                            
+                            file_repo_config = os.path.join(PATH_REPO,"config")
                             # Init Repository and check PATH_SRC exist
-                            if not os.path.isfile(PATH_REPO+SLASH+"config"):
+                            if not os.path.isfile(file_repo_config):
                                 if os.path.isdir(PATH_SRC):
                                     WriteLogFile(flog,f"{head_log};INIT")
                                     result = ResticExec(RESTIC_EXE,PATH_SRC,PATH_REPO,"init")
@@ -118,7 +136,7 @@ else:
                                 WriteLogFile(flog,f"{head_log};{result}")
 
                             # Action if Repository exist
-                            if os.path.isfile(PATH_REPO+SLASH+"config"):
+                            if os.path.isfile(file_repo_config):
                                 if PARAM == 'BACKUP':
                                     result = ResticExec(RESTIC_EXE,PATH_SRC,PATH_REPO,"backup")
                                     WriteLogFile(flog,f"{head_log};{result}")
@@ -127,8 +145,10 @@ else:
                                     snapshots = json.loads(json_result)
                                     for snapshot in snapshots:
                                         snapshot_id = snapshot["short_id"]
-                                        date_obj = datetime.fromisoformat(snapshot["time"])
-                                        snapshot_date = date_obj.strftime("%d-%m-%Y %H:%M")
+                                        iso_string = snapshot["time"]
+                                        iso_date = datetime.fromisoformat(iso_string[:19])
+                                        snapshot_date = iso_date.strftime("%d-%m-%Y %H:%M")
+                                        
                                         WriteLogFile(flog,f"{head_log};SNAPSHOT: {snapshot_id} = {snapshot_date}")
 
                                     size_gb = GetDirSize(PATH_REPO)
